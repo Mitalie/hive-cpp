0d84f139a68aa678db7db4c922d603e72283e334
Anssi Mäkinen <amakinen@student.hive.fi>
2025-09-05 17:35:19 +0300

    [m01/ex03] Implement and test the classes

---
+++ m01/ex03/Weapon.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+class Weapon
+{
+public:
+	Weapon(const std::string &type);
+	const std::string &getType() const;
+	void setType(const std::string &newType);
+
+private:
+	std::string type;
+};
