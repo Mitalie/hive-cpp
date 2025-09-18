871f94a4eb4edff1c9c103bfba278a475e606477
Anssi Mäkinen <amakinen@student.hive.fi>
2025-09-18 21:48:11 +0300

    [m01/ex05] Implement and test Harl class

---
+++ m01/ex05/main.cpp
@@ -0,0 +1,22 @@
+#include "Harl.hpp"
+
+#include <iostream>
+
+int main()
+{
+	Harl harl;
+
+	std::cout << "==== Test complaints in default order (D-I-W-E) ====" << std::endl;
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+
+	std::cout << "==== Test complaints in modified order (E-I-D-W) ====" << std::endl;
+	harl.complain("ERROR");
+	harl.complain("INFO");
+	harl.complain("DEBUG");
+	harl.complain("WARNING");
+
+	std::cout << "==== Done ====" << std::endl;
+}
