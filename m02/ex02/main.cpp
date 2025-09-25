02a2c8de869b5ca08d0888d117c41fd4693f4458
Anssi Mäkinen <amakinen@student.hive.fi>
2025-09-25 16:46:27 +0300

    [m02/ex02] Add more tests

--- m02/ex02/main.cpp
+++ m02/ex02/main.cpp
@@ -2,7 +2,7 @@
 
 #include "Fixed.hpp"
 
-int main(void)
+void test1()
 {
 	Fixed a;
 	Fixed const b(Fixed(5.05f) * Fixed(2));
@@ -14,6 +14,54 @@ int main(void)
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << Fixed::max(a, b) << std::endl;
+}
 
-	return 0;
+void test2()
+{
+	Fixed const a(2.5f);
+	Fixed b(a);
+
+	std::cout << "a = " << a << " ; b = " << b << "\n\n";
+
+	std::cout << "    b++ : " << b++ << "\n";
+	std::cout << "      b : " << b << "\n";
+	std::cout << "  a + b : " << a + b << "\n";
+	std::cout << "  a - b : " << a - b << "\n";
+	std::cout << "  a * b : " << a * b << "\n";
+	std::cout << "  a / 2 : " << a / 2 << "\n";
+	std::cout << "  b / 2 : " << a / 2 << "\n";
+	std::cout << "++b / 2 : " << ++b / 2 << "\n";
+	std::cout << "    --b : " << --b << "\n\n";
+
+	std::cout << "a = " << a << " ; b = " << b << "\n\n";
+
+	std::cout << " a < b : " << (a < b) << "\n";
+	std::cout << " a > b : " << (a > b) << "\n";
+	std::cout << "a <= b : " << (a <= b) << "\n";
+	std::cout << "a >= b : " << (a >= b) << "\n";
+	std::cout << "a == b : " << (a == b) << "\n";
+	std::cout << "a != b : " << (a != b) << "\n\n";
+
+	std::cout << " a < a : " << (a < a) << "\n";
+	std::cout << " a > a : " << (a > a) << "\n";
+	std::cout << "a <= a : " << (a <= a) << "\n";
+	std::cout << "a >= a : " << (a >= a) << "\n";
+	std::cout << "a == a : " << (a == a) << "\n";
+	std::cout << "a != a : " << (a != a) << "\n\n";
+
+	std::cout << " b < a : " << (b < a) << "\n";
+	std::cout << " b > a : " << (b > a) << "\n";
+	std::cout << "b <= a : " << (b <= a) << "\n";
+	std::cout << "b >= a : " << (b >= a) << "\n";
+	std::cout << "b == a : " << (b == a) << "\n";
+	std::cout << "b != a : " << (b != a) << "\n\n";
+}
+
+int main()
+{
+	std::cout << "==== Run tests from assignment ====\n\n";
+	test1();
+	std::cout << "\n==== Run additional tests ====\n\n";
+	test2();
+	std::cout << "\n==== Done ====" << std::endl;
 }
