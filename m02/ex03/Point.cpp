733ea6cc94d6b8c717575e9cb9e2b2e1de579264
Anssi Mäkinen <amakinen@student.hive.fi>
2025-09-29 18:13:23 +0300

    [m02/ex03] Implement Point class

---
+++ m02/ex03/Point.cpp
@@ -0,0 +1,32 @@
+#include "Point.hpp"
+
+#include "Fixed.hpp"
+
+Point::Point()
+	: x(0), y(0)
+{
+}
+
+Point::Point(float const x, float const y)
+	: x(x), y(y)
+{
+}
+
+Point::Point(Point const &other)
+	: x(other.x), y(other.y)
+{
+}
+
+Point::~Point()
+{
+}
+
+Fixed const &Point::getX() const
+{
+	return x;
+}
+
+Fixed const &Point::getY() const
+{
+	return y;
+}
