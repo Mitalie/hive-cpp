e963d3fa5c16fbe85fae547feb7322abb2dc0df2
Anssi Mäkinen <amakinen@student.hive.fi>
2025-09-30 17:42:38 +0300

    [m02/ex03] Remove noisy debug prints in Fixed

--- m02/ex03/Fixed.cpp
+++ m02/ex03/Fixed.cpp
@@ -6,48 +6,40 @@
 Fixed::Fixed()
 	: bits(0)
 {
-	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(Fixed const &other)
 	: bits(other.bits)
 {
-	std::cout << "Copy constructor called" << std::endl;
 }
 
 Fixed::Fixed(int const value)
 	: bits(value << numFracBits)
 {
-	std::cout << "Int constructor called" << std::endl;
 }
 
 Fixed::Fixed(float const value)
 	: bits(roundf(value * (1 << numFracBits)))
 {
-	std::cout << "Float constructor called" << std::endl;
 }
 
 Fixed &Fixed::operator=(Fixed const &other)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
 	bits = other.bits;
 	return *this;
 }
 
 Fixed::~Fixed()
 {
-	std::cout << "Destructor called" << std::endl;
 }
 
 int Fixed::getRawBits() const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return bits;
 }
 
 void Fixed::setRawBits(int const raw)
 {
-	std::cout << "setRawBits member function called" << std::endl;
 	bits = raw;
 }
 
