8501f1991f779e62439f0708391510354d7b9363
Anssi Mäkinen <amakinen@student.hive.fi>
2025-10-02 18:45:34 +0300

    [m03/ex01] Copy files from ex00

---
+++ m03/ex01/ClapTrap.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+
+class ClapTrap
+{
+public:
+	ClapTrap();
+	ClapTrap(std::string const &name);
+	ClapTrap(ClapTrap const &other);
+	~ClapTrap();
+	ClapTrap &operator=(ClapTrap const &other);
+
+	void attack(std::string const &target);
+	void takeDamage(unsigned int const amount);
+	void beRepaired(unsigned int const amount);
+
+private:
+	std::string name;
+	unsigned int hitPoints;
+	unsigned int energy;
+	unsigned int attackDamage;
+};
