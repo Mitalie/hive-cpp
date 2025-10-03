9e8b3f92549edfb9345769cba3cd5ec11677df6f
Anssi Mäkinen <amakinen@student.hive.fi>
2025-10-03 17:51:51 +0300

    [m03/ex03] Copy files from ex02

---
+++ m03/ex03/ScavTrap.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+public:
+	ScavTrap();
+	ScavTrap(std::string const &name);
+	ScavTrap(ScavTrap const &other);
+	~ScavTrap();
+	ScavTrap &operator=(ScavTrap const &other);
+
+	void attack(std::string const &target);
+	void guardGate();
+};
