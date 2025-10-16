4ad9c0944ed2314911fcbf879b4b0be840bc4abf
Anssi Mäkinen <amakinen@student.hive.fi>
2025-10-16 19:31:02 +0300

    [m04/ex03] Add inventory to Character
    Implement inventory cleanup and deep copy

--- m04/ex03/Character.cpp
+++ m04/ex03/Character.cpp
@@ -9,30 +9,46 @@
 Character::~Character()
 {
 	std::cout << "Destructing Character \"" << name << "\"\n";
+	for (unsigned int i = 0; i < invSize; ++i)
+		delete inventory[i];
 }
 
 Character::Character()
-	: name("<unnamed>")
+	: name("<unnamed>"),
+	  inventory()
 {
 	std::cout << "Default-constructing Character \"" << name << "\"\n";
 }
 
 Character::Character(std::string const &name)
-	: name(name)
+	: name(name),
+	  inventory()
 {
 	std::cout << "Constructing Character \"" << name << "\"\n";
 }
 
 Character::Character(Character const &other)
-	: name(other.name)
+	: name(other.name),
+	  inventory()
 {
 	std::cout << "Copy-constructing Character \"" << name << "\"\n";
+	for (unsigned int i = 0; i < invSize; ++i)
+		if (other.inventory[i])
+			inventory[i] = other.inventory[i]->clone();
 }
 
 Character &Character::operator=(Character const &other)
 {
 	std::cout << "Copy-assigning Character \"" << other.name << "\" over \"" << name << "\"\n";
 	name = other.name;
+	for (unsigned int i = 0; i < invSize; ++i)
+	{
+		delete inventory[i];
+		inventory[i] = 0;
+	}
+	for (unsigned int i = 0; i < invSize; ++i)
+		if (other.inventory[i])
+			inventory[i] = other.inventory[i]->clone();
 	return *this;
 }
 
