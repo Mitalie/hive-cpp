db721ff7a98d4e979067326990ec0e38faa715c4
Anssi Mäkinen <amakinen@student.hive.fi>
2025-10-22 18:41:48 +0300

    [m05/ex00] Implement and test Bureaucrat

---
+++ m05/ex00/Bureaucrat.cpp
@@ -0,0 +1,64 @@
+#include "Bureaucrat.hpp"
+
+#include <ostream>
+#include <string>
+#include <stdexcept>
+
+Bureaucrat::GradeTooHighException::GradeTooHighException()
+	: std::logic_error("Bureaucrat grade too high")
+{
+}
+
+Bureaucrat::GradeTooLowException::GradeTooLowException()
+	: std::logic_error("Bureaucrat grade too low")
+{
+}
+
+Bureaucrat::~Bureaucrat()
+{
+}
+
+Bureaucrat::Bureaucrat(std::string const &name, int grade)
+	: name(name), grade(grade)
+{
+	if (grade < minGrade)
+		throw GradeTooHighException();
+	if (grade > maxGrade)
+		throw GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &other)
+	: name(other.name),
+	  grade(other.grade)
+{
+}
+
+std::string const &Bureaucrat::getName() const
+{
+	return name;
+}
+
+int Bureaucrat::getGrade() const
+{
+	return grade;
+}
+
+void Bureaucrat::promote()
+{
+	if (grade <= minGrade)
+		throw GradeTooHighException();
+	grade--;
+}
+
+void Bureaucrat::demote()
+{
+	if (grade >= maxGrade)
+		throw GradeTooLowException();
+	grade++;
+}
+
+std::ostream &operator<<(std::ostream &os, Bureaucrat const &b)
+{
+	os << b.getName() << ", bureaucrat grade " << b.getGrade() << ".";
+	return os;
+}
---
+++ m05/ex00/Bureaucrat.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iosfwd>
+#include <string>
+#include <stdexcept>
+
+class Bureaucrat
+{
+public:
+	~Bureaucrat();
+	Bureaucrat(std::string const &name, int grade);
+	Bureaucrat(Bureaucrat const &other);
+
+	class GradeTooHighException : public std::logic_error
+	{
+	public:
+		GradeTooHighException();
+	};
+	class GradeTooLowException : public std::logic_error
+	{
+	public:
+		GradeTooLowException();
+	};
+
+	std::string const &getName() const;
+	int getGrade() const;
+	void promote();
+	void demote();
+
+private:
+	// default constructor forbidden, name and grade required
+	Bureaucrat();
+	// assignment forbidden, name is const
+	Bureaucrat &operator=(Bureaucrat const &other);
+
+	std::string const name;
+	int grade;
+
+	static int const minGrade = 1;
+	static int const maxGrade = 150;
+};
+
+std::ostream &operator<<(std::ostream &os, Bureaucrat const &b);
