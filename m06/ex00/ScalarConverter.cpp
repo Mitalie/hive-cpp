aacce028b09c1fc68bb88c9e9b8c202e12087dda
Anssi Mäkinen <amakinen@student.hive.fi>
2026-04-10 19:18:24 +0300

    wip

--- m06/ex00/ScalarConverter.cpp
+++ m06/ex00/ScalarConverter.cpp
@@ -1,79 +1,46 @@
 #include "ScalarConverter.hpp"
 
+#include <cmath>
 #include <cstddef>
 #include <iostream>
 #include <limits>
 #include <stdexcept>
 #include <string>
 
-template <typename T>
-void printChar(T v)
-{
-	char cv = static_cast<char>(v);
-	if (v < 0 || v > std::numeric_limits<char>::max())
-		std::cout << "char: Impossible\n";
-	else if (cv < 32 || cv > 126)
-		std::cout << "char: Non-printable character\n";
-	else
-		std::cout << "char: '" << cv << "'\n";
-}
-
-template <typename T>
-void printInt(T v)
-{
-	int iv = static_cast<int>(v);
-	if (v < std::numeric_limits<int>::min() || v > (double)std::numeric_limits<int>::max())
-		std::cout << "int: Impossible\n";
-	else
-		std::cout << "int: " << iv << '\n';
-}
-
-template <typename T>
-void printFloat(T v)
-{
-	float fv = static_cast<float>(v);
-	if (v < std::numeric_limits<float>::min() || v > std::numeric_limits<float>::max())
-		std::cout << "float: Impossible\n";
-	else
-		std::cout << "float: " << fv << '\n';
-}
-
-template <typename T>
-void printDouble(T v)
-{
-	double dv = static_cast<double>(v);
-	std::cout << "double: " << dv << '\n';
-}
-
-template <typename T>
-void printAll(T v)
-{
-	printChar(v);
-	printInt(v);
-	printFloat(v);
-	printDouble(v);
-}
-
 bool handleCharLiteral(std::string const &input)
 {
 	// must be 3-char string that starts and ends with single quote
 	if (input.length() != 3 || input[0] != '\'' || input[2] != '\'')
 		return false;
-	printAll(input[1]);
+	char v = input[1];
+	if (v < 32 || v > 126)
+		std::cout << "char: Non-printable character\n";
+	else
+		std::cout << "char: '" << v << "'\n";
+	std::cout << "int: " << static_cast<int>(v) << '\n';
+	std::cout << "float: " << static_cast<float>(v) << '\n';
+	std::cout << "double: " << static_cast<double>(v) << '\n';
 	return true;
 }
 
 bool handleIntLiteral(std::string const &input)
 {
-	size_t parsedLen;
-	int value;
 	try
 	{
 		// must be entirely consumed by std::stoi
-		value = std::stoi(input, &parsedLen);
+		size_t parsedLen;
+		int v = std::stoi(input, &parsedLen);
 		if (parsedLen != input.length())
 			return false;
-		printAll(value);
+		if (v < 0 || v > std::numeric_limits<char>::max())
+			std::cout << "char: impossible\n";
+		else if (v < 32 || v > 126)
+			std::cout << "char: Non-printable character\n";
+		else
+			std::cout << "char: '" << static_cast<char>(v) << "'\n";
+		std::cout << "int: " << v << '\n';
+		std::cout << "float: " << static_cast<float>(v) << '\n';
+		std::cout << "double: " << static_cast<double>(v) << '\n';
 		return true;
 	}
 	catch (std::invalid_argument const &e)
@@ -91,15 +58,25 @@ bool handleFloatLiteral(std::string const &input)
 	// float literals always end with an extra 'f'
 	if (input.back() != 'f')
 		return false;
-	size_t parsedLen;
-	float value;
 	try
 	{
 		// must be entirely consumed by std::stof, except for the trailing 'f'
-		value = std::stof(input, &parsedLen);
+		size_t parsedLen;
+		float v = std::stof(input, &parsedLen);
 		if (parsedLen != input.length() - 1)
 			return false;
-		printAll(value);
+		if (std::isnan(v) || v < 0 || v > std::numeric_limits<char>::max())
+			std::cout << "char: impossible\n";
+		else if (v < 32 || v > 126)
+			std::cout << "char: Non-printable character\n";
+		else
+			std::cout << "char: '" << static_cast<char>(v) << "'\n";
+		if (std::isnan(v) || v < std::numeric_limits<int>::min() || v > (double)std::numeric_limits<int>::max())
+			std::cout << "int: impossible\n";
+		else
+			std::cout << "int: " << static_cast<int>(v) << '\n';
+		std::cout << "float: " << v << "f\n";
+		std::cout << "double: " << static_cast<double>(v) << '\n';
 		return true;
 	}
 	catch (std::invalid_argument const &e)
@@ -114,15 +91,29 @@ bool handleFloatLiteral(std::string const &input)
 
 bool handleDoubleLiteral(std::string const &input)
 {
-	size_t parsedLen;
-	double value;
 	try
 	{
 		// must be entirely consumed by std::stod
-		value = std::stod(input, &parsedLen);
+		size_t parsedLen;
+		double v = std::stod(input, &parsedLen);
 		if (parsedLen != input.length())
 			return false;
-		printAll(value);
+		if (std::isnan(v) || v < 0 || v > std::numeric_limits<char>::max())
+			std::cout << "char: impossible\n";
+		else if (v < 32 || v > 126)
+			std::cout << "char: Non-printable character\n";
+		else
+			std::cout << "char: '" << static_cast<char>(v) << "'\n";
+		if (std::isnan(v) || v < std::numeric_limits<int>::min() || v > (double)std::numeric_limits<int>::max())
+			std::cout << "int: impossible\n";
+		else
+			std::cout << "int: " << static_cast<int>(v) << '\n';
+		if (!std::isinf(v) && (v < std::numeric_limits<float>::min() || v > std::numeric_limits<float>::max()))
+			std::cout << "float: impossible\n";
+		else
+			std::cout << "float: " << v << "f\n";
+		std::cout << "double: " << static_cast<double>(v) << '\n';
+		return true;
 		return true;
 	}
 	catch (std::invalid_argument const &e)
