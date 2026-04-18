2fb16bc1feff48759306f9508b58dedec3d7bbee
Anssi Mäkinen <amakinen@student.hive.fi>
2026-04-18 15:07:26 +0300

    [m08/ex00] Handle const and non-const containers properly

--- m08/ex00/main.cpp
+++ m08/ex00/main.cpp
@@ -6,25 +6,41 @@
 
 #include "easyfind.hpp"
 
-template <typename T>
-void testFind(const T &container, int value, const char *containerName)
+// In C++98 we can't use auto to deduce the iterator type. Specify it as a second template
+// parameter for the actual implementation of testFind, and write a wrapper that specifies
+// it based on the container type, overloaded for const and non-const containers.
+
+template <typename Coll, typename It>
+void testFind_impl(Coll &container, int value, const char *containerName)
 {
 	std::cout << "Searching for " << value << " in " << containerName << "...\n";
 	std::cout << "    contents from begin():";
-	for (typename T::const_iterator it = container.begin(); it != container.end(); ++it)
+	for (It it = container.begin(); it != container.end(); ++it)
 		std::cout << ' ' << *it;
 	std::cout << '\n';
-	typename T::const_iterator found = easyfind(container, value);
+	It found = easyfind(container, value);
 	if (found == container.end())
 		std::cout << "    easyfind() returned end() - value not found\n";
 	else
 	{
 		std::cout << "    easyfind() returned a non-end() iterator\n";
 		std::cout << "    contents from found position:";
-		for (typename T::const_iterator it = found; it != container.end(); ++it)
+		for (It it = found; it != container.end(); ++it)
 			std::cout << ' ' << *it;
 		std::cout << '\n';
 	}
+}
+
+template <typename Coll>
+void testFind(Coll &container, int value, const char *containerName)
+{
+	testFind_impl<Coll, typename Coll::iterator>(container, value, containerName);
+}
+
+template <typename Coll>
+void testFind(const Coll &container, int value, const char *containerName)
+{
+	testFind_impl<const Coll, typename Coll::const_iterator>(container, value, containerName);
 }
 
 int main()
@@ -34,11 +50,20 @@ int main()
 	std::vector<int> v(nums, nums + numCount);
 	std::list<int> l(nums, nums + numCount);
 	std::deque<int> d(nums, nums + numCount);
+	const std::vector<int> &vc = v;
+	const std::list<int> &lc = l;
+	const std::deque<int> &dc = d;
 
-	testFind(v, 3, "Vector");
-	testFind(v, 8, "Vector");
-	testFind(l, 3, "List");
-	testFind(l, 8, "List");
-	testFind(d, 3, "Deque");
-	testFind(d, 8, "Deque");
+	testFind(v, 3, "std::vector<int>");
+	testFind(v, 8, "std::vector<int>");
+	testFind(l, 3, "std::list<int>");
+	testFind(l, 8, "std::list<int>");
+	testFind(d, 3, "std::deque<int>");
+	testFind(d, 8, "std::deque<int>");
+	testFind(vc, 3, "const std::vector<int>");
+	testFind(vc, 8, "const std::vector<int>");
+	testFind(lc, 3, "const std::list<int>");
+	testFind(lc, 8, "const std::list<int>");
+	testFind(dc, 3, "const std::deque<int>");
+	testFind(dc, 8, "const std::deque<int>");
 }
